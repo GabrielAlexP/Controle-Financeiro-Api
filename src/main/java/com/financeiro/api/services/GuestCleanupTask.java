6d08f7571f56b351ff9611ef4eebceebbcd515bb
GabrielAlexP <bielpiresmorais@hotmail.com>
2026-03-12 21:08:27 -0300

    Correção de limpeza + ajuste de segurança

--- src/main/java/com/financeiro/api/services/GuestCleanupTask.java
+++ src/main/java/com/financeiro/api/services/GuestCleanupTask.java
@@ -15,14 +15,10 @@ public class GuestCleanupTask {
     private UserRepository userRepository;
 
     @Transactional
-    // Hack para testes: "0 * * * * *" significa rodar no segundo 0 de TODO MINUTO
-    @Scheduled(cron = "0 * * * * *")
+
+    @Scheduled(cron = "0 0 * * * *")
     public void cleanupGuestUsers() {
-        // Hack para testes: Considera "antigo" qualquer um criado há mais de 1 minuto (em vez de 2 horas)
-        LocalDateTime cutoff = LocalDateTime.now().minusMinutes(1);
+        LocalDateTime cutoff = LocalDateTime.now().minusHours(2);
         userRepository.deleteAllByIsGuestTrueAndCreatedAtBefore(cutoff);
-        
-        // Um print apenas para você ver o faxineiro passando no terminal do Spring!
-        System.out.println("🧹 [FAXINEIRO] Varredura executada às " + LocalDateTime.now());
     }
 }
